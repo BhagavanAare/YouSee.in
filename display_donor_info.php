fc69d364cb6ee9201cf3a76e4afe8a12b76cfd42
laxman <mittu.thefire@gmail.com>
2013-01-29 11:00:38 +0530

    shows displayname for any kind of user
    program is modified o conain a single line.. recommended to remove the
    file and include it somewhere else

--- display_donor_info.php
+++ display_donor_info.php
@@ -1,20 +1,5 @@
 <?php
 
-include("prod_conn.php");
-
-// this section generates query for donor info
-$query = "SELECT username
-          FROM users
-          WHERE user_id=".$_SESSION['SESS_USER_ID'];
-//echo "\n".$_SESSION['SESS_USER_ID'];
-//echo $query;
-mysql_connect("$dbhost","$dbuser","$dbpass");
-mysql_select_db("$dbdatabase");
-$result = mysql_query($query);
-
-
-//Display donor info
-while ($row = mysql_fetch_assoc($result)) { echo $row['username'];}
-
+echo $_SESSION['SESS_DISPLAYNAME'];
 ?>
 
