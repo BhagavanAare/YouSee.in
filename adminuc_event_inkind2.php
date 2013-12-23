c252f7c8ed7b7689080934733ef962f72f7d5c47
mittu-spidey <mittu.thefire@gmail.com>
2013-12-23 12:55:58 +0530

    new column place_id is added to table kind_donations

--- adminuc_event_inkind2.php
+++ adminuc_event_inkind2.php
@@ -16,6 +16,20 @@ mysql_select_db("$dbdatabase");
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 	<head>
+		<style>
+			.search_list
+			{
+				border:1px solid transpaent;
+				padding:1px;
+				margin:0px;
+			}
+			.search_list:hover
+			{
+				border:1px solid #ccc;
+				cursor:pointer;
+				font-weight:bold;
+			}
+		</style>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 		<link rel="stylesheet" type="text/css" href="css/main.css">
 		<link rel="stylesheet" type="text/css" href="css/tabs.css">
@@ -24,19 +38,19 @@ mysql_select_db("$dbdatabase");
 		<script src="scripts/jquery.ui.core.js"></script>
 		<script src="scripts/jquery.ui.widget.js"></script>
 		<script src="scripts/datepicker.js"></script>
-		<script src="scripts/update_waste.js"></script>
+		<script src="scripts/ajax_donor_utility.js"></script>
 		<script type="text/javascript">
-			$j(function() {
-				$j("#category").change(function() {
-					$j("#item option").show();
-					if ($j(this).data('itemoptions') == undefined) {
+			$(function() {
+				$("#category").change(function() {
+					$("#item option").show();
+					if ($(this).data('itemoptions') == undefined) {
 						/*Taking an array of all options-2 and kind of embedding it on the select1*/
-						$j(this).data('itemoptions', $j('#item option').clone());
+						$(this).data('itemoptions', $('#item option').clone());
 					}
-					var id = $j(this).val();
-					var itemoptions = $j(this).data('itemoptions').filter('[name=' + id + ']');
-					$j('#item').html(itemoptions);
-					$j('#item').prepend('<option value="" selected="selected" >--Select--</option>');
+					var id = $(this).val();
+					var itemoptions = $(this).data('itemoptions').filter('[name=' + id + ']');
+					$('#item').html(itemoptions);
+					$('#item').prepend('<option value="" selected="selected" >--Select--</option>');
 				});
 			});
 		</script>
@@ -56,6 +70,26 @@ mysql_select_db("$dbdatabase");
 			$npoCity[] = $row['hq_town_city'];
 
 		}
+
+		//Service places data
+
+		$placeIds = array();
+		$placeNames = array();
+		$placePartnerIds = array();
+		$placeLocations = array();
+		$placeCity = array();
+		$placeAddress = array();
+		$result = mysql_query('SELECT place_id, place_title, location, city, address, partner_id FROM places');
+		while ($row = mysql_fetch_array($result))
+		{
+			$placeIds[] = $row['place_id'];
+			$placeNames[] = $row['place_title'];
+			$placePartnerIds[] = $row['partner_id'];
+			$placeLocations[] = $row['location'];
+			$placeCity[] = $row['city'];
+			$placeAddress[] = $row['address'];
+		}
+
 		// ////////category data
 		$result = mysql_query('SELECT category,category_id FROM item_category');
 		$categoryIDs = array();
@@ -84,11 +118,11 @@ mysql_select_db("$dbdatabase");
 
 		<script type="text/javascript">
 
-$j(document).ready(function()
+			$(document).ready(function()
 {
 
 var i=0;
-$j("#addButton").click(function()
+$("#addButton").click(function()
 {
 var categoryData = "<tr id='itemRow"+i+"'><td align='right'>Category</td><td><select style='max-width: 120px;' type='text' id='category"+i+"' name='category[]' required> <option value=''>--SELECT--</option>";<?php
 
@@ -99,7 +133,7 @@ for ($i = 0; $i < count($categoryIDs); $i++)
 ?>
 	categoryData += "</select></td>";
 
-	var itemData = " <td align='right'>Item</td> <td><select style='max-width: 120px;' type='text' name='item[]' id='item" + i + "' required > <option value='' >--SELECT--</option>";
+	var itemData = " <td align='right'>Item</td> <td><select style='max-width: 120px;' type='text' name='item[]' id='item" + i + "' required > <option value='' >--SELECT--</option>"; 
 <?php
 for ($i = 0; $i < count($itemIDs); $i++)
 {
@@ -125,26 +159,26 @@ for ($i = 0; $i < count($itemIDs); $i++)
 
 	var removeButton = "<td align='right'><input type='button' id='removeButton" + i + "' value='x' /></td></tr>";
 
-	$j(categoryData + itemData + unitsType + requestQuntity + removeButton).clone().appendTo($j("#items"));
+	$(categoryData + itemData + unitsType + requestQuntity + removeButton).clone().appendTo($("#items"));
 	var category = "category" + i;
 	var item = "item" + i;
 	var removeButtonId = "removeButton" + i;
 	var itemRowId = "itemRow" + i;
-	$j("#" + category).change(function() {
+	$("#" + category).change(function() {
 
-		$j("#" + item + " option").show();
-		if ($j(this).data('itemoptions') == undefined) {
+		$("#" + item + " option").show();
+		if ($(this).data('itemoptions') == undefined) {
 			/*Taking an array of all options-2 and kind of embedding it on the select1*/
-			$j(this).data('itemoptions', $j("#" + item + " option").clone());
+			$(this).data('itemoptions', $("#" + item + " option").clone());
 		}
-		var id = $j(this).val();
-		var itemoptions = $j(this).data('itemoptions').filter('[name=' + id + ']');
-		$j('#' + item).html(itemoptions);
-		$j('#' + item).prepend('<option value="" selected="selected" >--Select--</option>');
+		var id = $(this).val();
+		var itemoptions = $(this).data('itemoptions').filter('[name=' + id + ']');
+		$('#' + item).html(itemoptions);
+		$('#' + item).prepend('<option value="" selected="selected" >--Select--</option>');
 	});
-	$j("#" + removeButtonId).click(function() {
+	$("#" + removeButtonId).click(function() {
 
-		$j("#" + itemRowId).remove();
+		$("#" + itemRowId).remove();
 	});
 	i++;
 
@@ -154,34 +188,70 @@ for ($i = 0; $i < count($itemIDs); $i++)
 		</script>
 		<script type="text/javascript">
 		
-																					// updating City and Address fields when an NPO is selected.
-			$j(document).ready(function() {
+																								// updating City and Address fields when an NPO is selected.
+			$(document).ready(function() {
+				
+				$("#opp_date").datepicker();
+				$("#opp_date").val($.datepicker.formatDate('yy/mm/dd', new Date()));
 				
 				var addressArray = [];
 				var cityArray = [];
+				var placeAddressArray = [];
+				var placeCityArray = [];
 				<?php
 				for ($i = 0; $i < count($npoAdress); $i++)
 				{
 					echo "addressArray['$npoID[$i]'] = '$npoAdress[$i]';";
 					echo "cityArray['$npoID[$i]'] = '$npoCity[$i]';";
 				}
+				for ($i = 0; $i < count($placeAddress); $i++)
+				{
+					echo "placeAddressArray['$placeIds[$i]'] = '$placeAddress[$i]';";
+					echo "placeCityArray['$placeIds[$i]'] = '$placeCity[$i]';";
+				}
 				?>
-					$j("#partner").change(function() {
-						var value = $j('#partner').val();
+
+					$("#partner").change(function() {
+						
+						$("#place option").show();
+						if ($(this).data('itemoptions') == undefined) {
+							/*Taking an array of all options-2 and kind of embedding it on the select1*/
+							$(this).data('itemoptions', $("#place option").clone());
+						}
+						var id = $(this).val();
+						console.log(id);
+						var itemoptions = $(this).data('itemoptions').filter('[name=' + id + ']');
+						$('#place').html(itemoptions);
+						$('#place').prepend('<option value="" selected="selected" >--Select--</option>');
+						
+						var value = $('#partner').val();
+						
 
 						if (value != '') {
 							var address = addressArray['' + value];
 							var city = cityArray['' + value];
 							console.log('' + value);
-							$j("#requestAddress").val('' + address);
-							$j("#requestCity").val('' + city);
+							$("#requestAddress").val('' + address);
+							$("#requestCity").val('' + city);
 						} else {
-							$j("#requestAddress").val('');
-							$j("#requestCity").val('');
+							$("#requestAddress").val('');
+							$("#requestCity").val('');
 						}
 
 					});
+					$("#place").change(function(){
+						var value = $('#place').val();
+						if(value!='')
+						{
+							var address = placeAddressArray['' + value];
+							var city = placeCityArray['' + value];
+							console.log('' + value);
+							$("#requestAddress").val('' + address);
+							$("#requestCity").val('' + city);
+						}	
 					});
+					});
+					
 		</script>
 		<title>My UC | Events - Inkind donations</title>
 
@@ -206,7 +276,7 @@ for ($i = 0; $i < count($itemIDs); $i++)
 								style="float: left; border-right: 1px solid #ccc;">
 									<th colspan="2" align="left"><h3>Update an In-Kind Donation</h3></th>
 									<tr>
-										<td align="right">NPO</td>
+										<td id="partnertd" align="right">NPO*</td>
 										<td colspan="4">
 										<select style="max-width: 700px;" type="text"
 										id="partner" name="partner_id" required>
@@ -224,19 +294,34 @@ for ($i = 0; $i < count($itemIDs); $i++)
 									<tr>
 										<td align="right">Service Place</td>
 										<td colspan="4">
-										<select  type="text" id="servicePlace" name="place_id" >
+										<select  type="text" id="place" name="place_id" >
 											<option value="">--SELECT--</option>
+											<?php 
+											$placeCount = count($placeIds);
+											for($i=0;$i<$placeCount;$i++)
+											{
+												echo "<option value='$placeIds[$i]' name='$placePartnerIds[$i]' hidden>$placeNames[$i], $placeLocations[$i]</option> ";
+											}
+											?>
 										</select>
 										</td>
 									</tr>
-									<tr>
-										<td align="right" >Donor</td>
+									<tr> 
+										<td align="right">Donor*</td> 
 										<td colspan="8">
-											<input  type = "text" id = "donors_input" class = "donors_input" list="donors_list" name="donors_input">
-											<select style="max-width: 500px " id = "donors_list">
-											</select>
-										</td>								
-									</tr>
+											<input type="text" id="donor_search" placeholder="Donor Name" size=20 name="donor_name" /> 
+											<input type="text" name="donor_id" id="donor_selected" hidden />
+<span id="donor_info" style="float:left" hidden><b>Donor Info</b><br /> 
+<input type="text"  name="donor_email" placeholder="Email" id="emailbox" readonly />
+<input type="text" name="donor_mobile" id="mobilebox" placeholder="Mobile" readonly />
+<input type="text" name="donor_city" name="citybox" placeholder="City" id="citybox" readonly />
+<input type="text" name="donor_org" id="orgbox" placeholder="Organization" readonly />
+</span>
+<div id="search_result" style="border: 1px solid #ccc;height:auto;background:white;max-height:300px;width:300px; overflow:auto;display:none;position:absolute;z-index:1000">
+</div>
+</div>
+</td>
+</tr>
 									<tbody id="items">
 										<tr id="itemRow">
 											<td align="right">Category</td>
@@ -286,11 +371,7 @@ for ($i = 0; $i < count($itemIDs); $i++)
 						</td>
 					</tr>
 					</tbody>
-					<tr>
-						<td align="right">Delivery Date*</td>
-						<td><input  type="text" name="dod" id="opp_date" maxlength="450" cols="40" rows="5"></textarea> </td>
-					</tr>
-							
+					
 					<tr>
 						<td align="right" colspan="8">
 						<input type="button"
@@ -298,14 +379,19 @@ for ($i = 0; $i < count($itemIDs); $i++)
 						</td>
 					</tr>
 					<tr>
-						<td align="right">Request City</td>
+						<td align="right">Delivery Date*</td>
+						<td><input  type="text" name="dod" id="opp_date" maxlength="450" cols="40" rows="5"></textarea> </td>
+					</tr>
+							
+					<tr>
+						<td align="right">Request City*</td>
 						<td colspan="4">
 						<input id="requestCity" type="text" name="request_city"
 						value="Hyderabad" required />
 						</td>
 					</tr>
 					<tr>
-						<td align="right">Request Address</td>
+						<td align="right">Request Address*</td>
 						<td colspan="4">						<textarea id="requestAddress" rows="3" value=""
 												name="request_address" required></textarea></td>
 					</tr>
@@ -340,24 +426,33 @@ if (isset ( $_POST ['request_submit'] )) {
 
 // connect to database
 include_once ("prod_conn.php");
-$partnerquery = "SELECT partner_id,name,partner_email from project_partners WHERE partner_id=" . $_POST ['partner_id'];
+$partnerId = $_POST['partner_id'];
+$partnerquery = "SELECT partner_id,name,partner_email,contact_first_name from project_partners WHERE partner_id=" . $_POST ['partner_id'];
 $partner = mysql_fetch_array ( mysql_query ( $partnerquery ) );
 $categoryArray = $_POST ['category'];
 $itemIDArray = $_POST ['item'];
 $unitsTypeArray = $_POST ['units_type'];
 $reqQuntityArray = $_POST ['request_quantity'];
+
+$place = $_POST['place_id'];
+$donorId = $_POST['donor_id'];
+$date = $_POST['dod'];
+$dod = date ( "Y-m-d", strtotime ( $_POST ['dod'] ) );
+$donorName = $_POST['donor_name'];
+$donorMail = $_POST['donor_email'];
+
 $tables = array ();
 $itemCount = count ( $categoryArray );
 
-for($j = 0; $j < $itemCount; $j ++) {
-
-$insert_inkind = "INSERT INTO kind_donations(initiative_type,partner_id,donor_id,item_id,units_type,request_quantity,offer_quantity,request_city,offer_city ,request_address,offer_address, request_date,offer_date, request_expiry_date,delivery_date, transport,note,status)
-VALUES ('0','$partner[partner_id]',138,'$itemIDArray[$j]','$unitsTypeArray[$j]', '$reqQuntityArray[$j]','$reqQuntityArray[$j]', '$_POST[request_city]','$_POST[request_city]', '$_POST[request_address]','$_POST[request_address]', CURDATE() , CURDATE() , '" . date ( "Y-m-d", strtotime ( $_POST ['req_exp_date'] ) ) . "',CURDATE(), '0','Donation Camps, Individual Donations.','Delivered')";
-$registration = mysql_query ( $insert_inkind );
-$subid = mysql_insert_id ();
-$update = mysql_query ( "UPDATE kind_donations set sub_id=$subid WHERE donation_id=$subid" );
-$item = mysql_fetch_array ( mysql_query ( "SELECT donationitem,request_quantity from items INNER JOIN item_category on items.category_id=item_category.category_id INNER JOIN kind_donations ON items.item_id=kind_donations.item_id  WHERE kind_donations.donation_id=$subid" ) );
-$tables [] = "
+for($j = 0; $j < $itemCount; $j ++) 
+{
+	$insert_inkind = "INSERT INTO kind_donations(initiative_type,partner_id,donor_id,item_id,units_type,request_quantity,offer_quantity,request_city,offer_city ,request_address,offer_address, request_date,offer_date, request_expiry_date,delivery_date, transport,note,status,place_id)
+				VALUES ('0','$partner[partner_id]','$donorId','$itemIDArray[$j]','$unitsTypeArray[$j]', '$reqQuntityArray[$j]','$reqQuntityArray[$j]', '$_POST[request_city]','$_POST[request_city]', '$_POST[request_address]', '$_POST[request_address]', $dod , $dod , '" . date ( "Y-m-d", strtotime ( $_POST ['req_exp_date'] ) ) . "',$dod, '0','Donation Camps, Individual Donations.','Delivered','$place')";
+	$registration = mysql_query ( $insert_inkind );
+	$subid = mysql_insert_id ();
+	$update = mysql_query ( "UPDATE kind_donations set sub_id=$subid WHERE donation_id=$subid" );
+	$item = mysql_fetch_array ( mysql_query ( "SELECT donationitem,request_quantity from items INNER JOIN item_category on items.category_id=item_category.category_id INNER JOIN kind_donations ON items.item_id=kind_donations.item_id  WHERE kind_donations.donation_id=$subid" ) );
+	$tables [] = "
 <table cellpadding='5' style='border-collapse:collapse;' border='1'>
 <tr>
 <th colspan=\"2\" align=\"center\" height=\"20\" valign=\"top\">Request Details</th>
@@ -381,8 +476,26 @@ $tables [] = "
 
 $partnerEmail = $partner ['partner_email'];
 $parnterName = $partner ['name'];
-$emailSubject = "";
-$emailBody = "";
+$partnerContactName = $partner['contact_first_name'];
+$emailBody = "To,
+Dear $partnerContactName,
+
+This is to inform you that the following items have been received by " . $partner ['name'];
+
+if($place != '')
+{
+	$emailBody .= ", ".$placeNames[$partnerId];
+}
+$emailBody .=  " donated by $donorName on $date";
+
+for($i=0;$i<count($tables);$i++)
+{
+	$emailBody .= $tables[$i];
+}
+//echo "$emailBody";
+
+
+
 // sendEmail ( $partnerEmail, $parnterName, $emailSubject, emailBody );
 
 				?>
@@ -425,7 +538,7 @@ $emailBody = "";
 </html>
 
 <?php
-function sendEmail($email, $displayName, $subject, $mailBody)
+function sendEmail($email, $donorMail, $displayName, $mailBody)
 {
 	// global $string, $mailText;
 	require_once ("Email/class.phpmailer.php");
@@ -434,12 +547,13 @@ function sendEmail($email, $displayName, $subject, $mailBody)
 	{
 		$to = $email;
 		$mail -> AddAddress($to);
+		$mail -> AddCC($donorMail);
 		$mail -> AddBCC("contact@yousee.in");
 
 		$body = "Dear  " . $displayName . ",<br><br>";
 		$body .= $mailBody;
 		$body .= "<br><br><br> From YouSee  (+91-8008-884422) <br /> <a href=\"www.yousee.in\">www.yousee.in</a>";
-		$mail -> Subject = $subject;
+		$mail -> Subject = "In-Kind Donations Recieved";
 		$mail -> Body = $body;
 		if ($mail -> Send())
 		{;
